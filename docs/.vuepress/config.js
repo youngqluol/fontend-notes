dae345d8ca4bba18d3f1c7f9a518fb4ecd70e67f
yongqing_luo <yongqing_luo@kingdee.com>
2021-11-16 17:56:41 +0800

    init docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'youngqluol\'s blogs',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.jpg' }]
+  ],
+  themeConfig: {
+    logo: '/logo.jpg',
+    sidebar: 'auto',
+    lastUpdated: 'Last Updated',
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,6 @@
+---
+home: true
+heroImage: /images/head.jpg
+heroText: youngluol的个人博客
+tagline: 种一棵树最好的时间是十年前，其次是九年前
+---
